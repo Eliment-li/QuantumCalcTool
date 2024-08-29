9b36102d5eb871b637efb5fd52a9327efba4c86f
Eliment-li <felix.litian@foxmail.com>
2024-08-29 17:06:55 +0800

    modify kroneckerProduct now can accept 0 as g 1 as e

--- calc/kroneckerProduct.py
+++ calc/kroneckerProduct.py
@@ -1,18 +1,30 @@
 import numpy as np
 from pprint import pprint
 import sympy as sp
-
+e = sp.Matrix([0, 1], dtype=int)
+g = sp.Matrix([1, 0], dtype=int)
 
 #test
 #np.kron(g, e)
 '''
-计算张量积，又叫直积或 Tensor Product, Kronecker Product,
-多个对象可放在一个数组里， 例如 items = [g, g, g, g]
+本函数计算张量积，又叫直积或 Tensor Product, Kronecker Product,
+可以用 0 代表 g, 1 代表 e
+多个对象可放在一个数组里， 例如 items = [g, g, g, g], 则计算 |gggg>
 '''
 def kroneckerProduct(*args):
     items = list(args)
+    #获取第一个项
     result = items[0]
+    if result == 0:
+        result = g
+    if result == 1:
+        result = e
+    #和后面的项依次做张量运算
     for i in range(1, len(items)):
+        if items[i] == 0:
+            items[i] = g
+        if items[i] == 1:
+            items[i] = e
         result = sp.kronecker_product(result, items[i])
     return result
 
