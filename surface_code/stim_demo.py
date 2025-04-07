d2b5d3e8bdc5fbde29a905f80342b91b1e5ee7ee
Eliment-li <felix.litian@foxmail.com>
2025-04-07 16:46:10 +0800

    --

--- surface_code/stim_demo.py
+++ surface_code/stim_demo.py
@@ -6,14 +6,13 @@ print(stim.__version__)
 circuit = stim.Circuit()
 
 # First, the circuit will initialize a Bell pair.
-circuit.append("H", [0])
 circuit.append("CNOT", [0, 1])
 
 # Then, the circuit will measure both qubits of the Bell pair in the Z basis.
 circuit.append("M", [0, 1])
 
 print(circuit.diagram())
-# circuit.diagram('timeline-svg')
+print(circuit.diagram('timeslice-svg-html'))
 
 sampler = circuit.compile_sampler()
 print(sampler.sample(shots=10))
